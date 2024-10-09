6a15c3091122af2cf401ba6398a091e84f94a6a6
leovolving <leo@leoyockey.com>
2024-10-09 12:05:25 -0400

    mobile header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
+import Icon from "../Icon";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import UnstyledButton from "../UnstyledButton";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -29,7 +31,18 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+        <IconButtonWrapper>
+          <UnstyledButton>
+            <Icon id="shopping-bag" />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="search" />
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="menu" />
+          </UnstyledButton>
+        </IconButtonWrapper>
+        <RightSide />
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +59,43 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.tabletDown} {
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletDown} {
+    display: none;
+  }
+`;
+
+const IconButtonWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletDown} {
+    display: flex;
+    gap: 16px;
+  }
+
+  @media ${QUERIES.phoneDown} {
+    gap: 8px;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
+`;
+
+const RightSide = styled(Side)`
+  @media ${QUERIES.tabletDown} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
